e15fb8963b32fce70352fd5ec73ad248a28e36b6
Andrew M <136806756+andrewmcs@users.noreply.github.com>
2023-09-20 18:02:06 +0100

    Change grid size
    - Adds a button for choosing a grid size.
    - Validates input (requires numeric < 100)
    - Clears any existing grid
    - Generates a new grid with given size
    - Encapsulates the grid generation code into a function.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,56 @@
+function isNumeric(str) {
+    if (typeof str != "string") return false // we only process strings!  
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+        !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
+
 window.onload = function () {
-    let sketchGrid = document.getElementById("sketch-grid");
-    console.log(sketchGrid);
-    for (let i = 0; i < 16**2; i++) {
-        var sketchElement = document.createElement("div");
-        sketchElement.setAttribute('class', 'sketch-element');
-        sketchElement.textContent=i;
-        sketchGrid.appendChild(sketchElement);
+
+    function generateGrid(gridSize) {
+        let sketchGrid = document.getElementById("sketch-grid");
+        console.log(sketchGrid);
+        for (let i = 0; i < gridSize ** 2; i++) {
+            flexBasisValue=100/gridSize;
+
+            var sketchElement = document.createElement("div");
+            sketchElement.setAttribute('class', 'sketch-element');
+            sketchElement.style.flexBasis=`${flexBasisValue-0.5}%`;
+            sketchElement.textContent = i;
+            sketchGrid.appendChild(sketchElement);
+        }
     }
+
+    generateGrid(5);
+
+    document.querySelectorAll('.sketch-element').forEach(sketchElement => {
+        sketchElement.addEventListener('mouseenter', event => {
+            //handle mouse enter
+            sketchElement.style.border = "1px dotted orange";
+        })
+    });
+    let changeGridButton = document.getElementById("change-grid-size");
+    console.log(changeGridButton);
+
+
+    document.getElementById("change-grid-size").addEventListener(
+        'click', function (event) {
+            let gridSizeRequest = prompt("What grid size would you like (per side)?");
+            console.log(typeof (gridSizeRequest));
+            if (gridSizeRequest >= 100 || !isNumeric(gridSizeRequest)) {
+                alert("Please choose a number less than 100 for the grid size.")
+            }
+            else {
+                let sketchGrid = document.getElementById("sketch-grid");
+                while (sketchGrid.firstChild) {
+                    sketchGrid.removeChild(sketchGrid.firstChild);
+                }
+
+            }
+        }
+    );
+
+
+
+
 
 }
