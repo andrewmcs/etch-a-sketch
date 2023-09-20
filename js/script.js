a110b9e40f0820d0920951731e74e8f044d75e3c
Andrew M <136806756+andrewmcs@users.noreply.github.com>
2023-09-20 18:23:21 +0100

    Fix bugs for previous commit, add coloured pixels
    - Fixes issues with flex-basis calculation
    - Adds random colour for grid elements on hover
    - Adds border for grid

--- js/script.js
+++ js/script.js
@@ -4,38 +4,50 @@ function isNumeric(str) {
         !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
 }
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function randomHsl() {
+    return 'hsla(' + (Math.random() * 360) + ', 100%, 50%, 1)';
+}
+
 window.onload = function () {
 
     function generateGrid(gridSize) {
         let sketchGrid = document.getElementById("sketch-grid");
-        console.log(sketchGrid);
         for (let i = 0; i < gridSize ** 2; i++) {
-            flexBasisValue=100/gridSize;
+            flexBasisValue = 100 / gridSize;
 
             var sketchElement = document.createElement("div");
             sketchElement.setAttribute('class', 'sketch-element');
-            sketchElement.style.flexBasis=`${flexBasisValue-0.5}%`;
-            sketchElement.textContent = i;
+            sketchElement.style.flexBasis = `${flexBasisValue}%`;
+            //sketchElement.textContent = i;
             sketchGrid.appendChild(sketchElement);
         }
+
+        document.querySelectorAll('.sketch-element').forEach(sketchElement => {
+            sketchElement.addEventListener('mouseenter', event => {
+                //handle mouse enter
+                sketchElement.style.backgroundColor = randomHsl();
+            })
+        });
     }
 
-    generateGrid(5);
+    // set initial grid size to be 16*16
+    generateGrid(16);
 
-    document.querySelectorAll('.sketch-element').forEach(sketchElement => {
-        sketchElement.addEventListener('mouseenter', event => {
-            //handle mouse enter
-            sketchElement.style.border = "1px dotted orange";
-        })
-    });
     let changeGridButton = document.getElementById("change-grid-size");
     console.log(changeGridButton);
-
 
     document.getElementById("change-grid-size").addEventListener(
         'click', function (event) {
             let gridSizeRequest = prompt("What grid size would you like (per side)?");
-            console.log(typeof (gridSizeRequest));
             if (gridSizeRequest >= 100 || !isNumeric(gridSizeRequest)) {
                 alert("Please choose a number less than 100 for the grid size.")
             }
@@ -44,7 +56,7 @@ window.onload = function () {
                 while (sketchGrid.firstChild) {
                     sketchGrid.removeChild(sketchGrid.firstChild);
                 }
-
+                generateGrid(gridSizeRequest);
             }
         }
     );
